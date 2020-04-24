fa02fc3594b1b425be5ea78e0920c3d7062534cf
Alex <alex@lanp.co>
2020-04-24 18:26:50 +0300

    Minor fixes

--- src/redux/AC/index.js
+++ src/redux/AC/index.js
@@ -1,78 +1,80 @@
 import {
-    LOAD_ALL_MOVIES, LOAD_MOVIE, ADD_TO_WATCHLIST,
-    DELETE_FROM_WATCHLIST, GET_FAVORITES_MOVIES
-} from '../constants'
-import axios from 'axios'
+  LOAD_ALL_MOVIES,
+  LOAD_MOVIE,
+  ADD_TO_WATCHLIST,
+  DELETE_FROM_WATCHLIST,
+  GET_FAVORITES_MOVIES,
+} from "../constants";
+import axios from "axios";
 
-const callApi = 'http://www.omdbapi.com'
+const callApi = "https://www.omdbapi.com";
 
 export const getFilms = (title) => {
-    return {
-        type: LOAD_ALL_MOVIES,
-        title, callMoviesAPI: callApi
-    }
-}
+  return {
+    type: LOAD_ALL_MOVIES,
+    title,
+    callMoviesAPI: callApi,
+  };
+};
 
 export const getFilm = (id) => {
-    return {
-        type: LOAD_MOVIE,
-        id, callMovieAPI: callApi
-    }
-}
+  return {
+    type: LOAD_MOVIE,
+    id,
+    callMovieAPI: callApi,
+  };
+};
 
 export const addToWatchlist = (id) => {
-    return {
-        type: ADD_TO_WATCHLIST,
-        payload: { id }
-    }
-}
+  return {
+    type: ADD_TO_WATCHLIST,
+    payload: { id },
+  };
+};
 
 export const deleteFromWatchlist = (id) => {
-    return {
-        type: DELETE_FROM_WATCHLIST,
-        payload: { id }
-    }
-}
+  return {
+    type: DELETE_FROM_WATCHLIST,
+    payload: { id },
+  };
+};
 
 export const getFavoritesMovies = (favoritesArr) => {
-    return {
-        type: GET_FAVORITES_MOVIES,
-        payload: { favorites: favoritesArr }
-    }
-}
-
-
+  return {
+    type: GET_FAVORITES_MOVIES,
+    payload: { favorites: favoritesArr },
+  };
+};
 
 export const getFavoritesMoviesThunk = (ids) => {
-    return (dispatch) => {
+  return (dispatch) => {
+    const favoritesArr = [];
 
-        const favoritesArr = []
+    const mapingArr = ids.map((id) => {
+      const options = {
+        method: "get",
+        url: callApi,
+        params: {
+          apiKey: "9e432373",
+          i: id,
+        },
+        transformResponse: [
+          (data) => {
+            const result = JSON.parse(data);
+            favoritesArr.push(result);
+            dispatch(getFavoritesMovies(favoritesArr));
+          },
+        ],
+      };
 
-        const mapingArr = ids.map(id => {
-            const options = {
-                method: "get",
-                url: callApi,
-                params: {
-                    apiKey: "9e432373",
-                    i: id,
-                },
-                transformResponse: [
-                    (data) => {
-                        const result = JSON.parse(data)
-                        favoritesArr.push(result)
-                        dispatch(getFavoritesMovies(favoritesArr))
-                    },
-                ],
-            };
+      axios(options);
+      console.log(favoritesArr);
+      return favoritesArr;
+    });
 
-            axios(options);
-            console.log(favoritesArr)
-            return favoritesArr;
-        })
-
-        return favoritesArr
-    }
-}
+    return favoritesArr;
+  };
+};
 
 // const instance = axios.create({
 //     withCredentials: true,
@@ -94,4 +96,3 @@ export const getFavoritesMoviesThunk = (ids) => {
 //         })
 //      }
 // }
-
